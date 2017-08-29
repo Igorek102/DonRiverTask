da20f20d61cc9c460a4593b79d07668f16503070
Igorek102 <igorek102161@gmail.com>
2017-08-30 01:34:19 +0300

    Update content.component.ts

--- src/app/content.component.ts
+++ src/app/content.component.ts
@@ -115,7 +115,7 @@ export class ContentComponent {
     const data: Blob = new Blob([buffer], {
       type: EXCEL_TYPE
     });
-    FileSaver.saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
+    FileSaver.saveAs(data, fileName + EXCEL_EXTENSION);
   }
 
   showDialogToExport() {
